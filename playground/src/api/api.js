c6ce2744d0eb2099fb7fab5d7cb6b2ace3c4de73
Irene Wang <lohas1107@gmail.com>
2025-01-20 22:17:46 +0000

    ♻️ refactor: get products from api

--- playground/src/api/api.js
+++ playground/src/api/api.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
 const API_BASE_URL = 'https://ec-course-api.hexschool.io';
+const API_PATH = 'irene';
 
 export const api = {
   signIn: (formData) => axios.post(`${API_BASE_URL}/v2/admin/signin`, formData),
   checkLogin: () => axios.post(`${API_BASE_URL}/v2/api/user/check`),
+
+  getProducts: () => axios.get(`${API_BASE_URL}/v2/api/${API_PATH}/admin/products`),
 };
