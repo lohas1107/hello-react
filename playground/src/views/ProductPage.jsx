36e17edc38d6f6e6dfdaaf2e35def228c4f8e786
Irene Wang <lohas1107@gmail.com>
2025-01-25 03:35:42 +0000

    📱 Adjust product page layout with responsive columns

--- playground/src/views/ProductPage.jsx
+++ playground/src/views/ProductPage.jsx
@@ -22,11 +22,11 @@ function ProductPage() {
         ? (
           <div className="container">
             <div className="row mt-5">
-              <div className="col-md-5">
+              <div className="col-md-6">
                 <h2>產品列表</h2>
                 <ProductList selectItemHandler={setSelectedProduct} />
               </div>
-              <div className="col-md-7">
+              <div className="col-md-6">
                 <h2>產品細節</h2>
                 <ProductDetail detail={selectedProduct} />
               </div>
